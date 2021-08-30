e161628e6b653309a4d78b07f9bee34399f3b3bf
Chang-ji <75286090+Chang-ji@users.noreply.github.com>
2021-08-30 23:20:02 +0900

    first commit

---
+++ src/test/java/com/fastcampus/jpa/bookmanager/domain/UserTest.java
@@ -0,0 +1,24 @@
+package com.fastcampus.jpa.bookmanager.domain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+class UserTest {
+
+    @Test
+    void Test() {
+        User user1 = new User("martin", "martin@fastcampus", LocalDateTime.now(), LocalDateTime.now());
+        User user2 = new User("martin", "martin@fastcampus");
+        User user3 = User.builder()
+                .name("martin")
+                .email("martin@fastcampus.com")
+                .build();
+
+        System.out.printf(">>> " + user1.toString());
+        System.out.printf(">>> " + user2.toString());
+        System.out.printf(">>> " + user3.toString());
+    }
+}
